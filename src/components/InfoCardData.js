4bb357f4431079de2a8d9399a9013419c6b409c7
Daniel Dahl <danieldahl99@gmail.com>
2021-04-24 17:38:49 +0200

    Updated text of github and bitbucket entry

--- src/components/InfoCardData.js
+++ src/components/InfoCardData.js
@@ -20,13 +20,13 @@ const InfoCardData = [
 		title : "GitHub",
 		icon  : github_logo,
 		text  :
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			"GitHub is what I primarily use for my projects. On my GitHub profile I have a range of projects from different topics such as assignments from the graphics programming course, game jams from rapid prototyping course and my personal Discord bot written in Go."
 	},
 	{
 		title : "Bitbucket",
 		icon  : bitbucket_logo,
 		text  :
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			"Bitbucket is what I mostly used whilst studying at NTNU. Here you can find some assignments related to some of the courses."
 	},
 	{
 		title : "Contact",
