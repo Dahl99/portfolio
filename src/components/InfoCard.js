057076fbd886b00b8d29916984f10f62c3e9d46f
Daniel Dahl <danieldahl99@gmail.com>
2021-04-24 17:09:36 +0200

    Moved infocard title to grid container

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -3,10 +3,12 @@ import "./InfoCard.css";
 const InfoCard = ({ title, icon, text }) => {
 	return (
 		<div className="InfoCard">
-			<h1 className="Card-title">{title}</h1>
 			<div className="Grid-container">
 				<img className="Card-image" src={icon} alt="Cannot display image" />
-				<p className="Card-text">{text}</p>
+				<div>
+					<h1 className="Card-title">{title}</h1>
+					<p className="Card-text">{text}</p>
+				</div>
 			</div>
 		</div>
 	);
